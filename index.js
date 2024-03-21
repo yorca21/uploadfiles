80461d1e7d0d495d2aa387d6dec5cd4862b334b0
yorca21 <haruka.kaede21@gmail.com>
2024-03-21 10:23:36 -0400

    cambios

---
+++ index.js
@@ -0,0 +1,38 @@
+ const bodyParser = require('body-parser')
+ const express = require('express')
+ const fileUpload = require ('express-fileupload')
+ const app = express()
+ const path = require('path')
+ const { v4 : uuidv4 } = require('uuid')
+
+ app.use(express.json())
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({extended: true}))
+
+ app.use(fileUpload());
+
+app.post('/', (req, res)=>{
+  if(!req.files || Object.keys(req.files).length === 0){
+     return res.status(400).send('No se cargó ningún archivo.');
+  }
+   const ArchivoEjemplo = req.files.archivo
+   const extension = ArchivoEjemplo.name.split('.')[ArchivoEjemplo.name.split('.').length -1]
+  
+   const ruta = path.join(__dirname, 'archivos', uuidv4() + '.' + extension)
+   ArchivoEjemplo.mv( ruta, (error) =>{
+     if(error)
+       return res.status(500).json({
+         status : 500 ,
+         mensaje : ' Error en el almacenaje del archivo',
+         error
+  })
+  return res.status(201).json({
+    status : 201,
+    mensaje : ' Archivo almacenado correctamente'
+   })
+ })
+  
+ })
+ app.listen(3000 , function() {
+   console.log('servidor es => https://localhost:3000')
+});
