a9d870029d3b62d26be267b3550cec44a5eb8de3
yorca21 <haruka.kaede21@gmail.com>
2024-03-22 13:12:30 -0400

    se pudo

--- index.js
+++ index.js
@@ -17,13 +17,15 @@
     return res.status(400).send('No se han enviado archivos.');
   }
 
-  const files = req.files.files;
+  const files = req.files.archivo;
   const filesArray = Array.isArray(files) ? files : [files];
 
   filesArray.forEach(file => {
     
-    const extension = file.name.split('.')[file.name.split('.').length - 1];
-    const ruta = path.join(__dirname, 'uploads', uuidv4() + '.' + extension)
+    if(file&&file.name){
+
+      const extension = file.name.split('.')[file.name.split('.').length - 1];
+      const ruta = path.join( __dirname, 'archivos', uuidv4() + '.' + extension)
     
     file.mv(ruta ,(error)  => {
        if(error)
@@ -34,12 +36,14 @@
         error
       })
     });
+
+    }
+    
   });
   return res.status(201).json({
     status : 201,
     mensaje : ' Archivo almacenado correctamente'
 });
-
 /*app.post('/', (req, res)=>{
   if(!req.files || Object.keys(req.files).length === 0){
     return res.status(400).send('No se cargó ningún archivo.')
