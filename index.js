1190b1eeca4bd51900cbe5bf43409501668d2abe
yorca21 <haruka.kaede21@gmail.com>
2024-03-25 10:17:36 -0400

    cambios

--- index.js
+++ index.js
@@ -1,72 +1,71 @@
- const bodyParser = require('body-parser')
- const express = require('express')
- const fileUpload = require ('express-fileupload')
- const app = express()
- const path = require('path')
- const { v4 : uuidv4 } = require('uuid')
+const bodyParser = require('body-parser')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
 
- app.use(express.json())
- app.use(bodyParser.json())
- app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
- 
- app.use(fileUpload({
-   useTempFiles: true,
-    tempFileDir: '/tmp/'
-  }));
- 
- app.post('/', (req, res) => {
-  
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+
+app.post('/', (req, res) => {
+
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No se han enviado archivos.');
   }
 
-  const file = req.files.archivo;
-  const filesArray = Array.isArray(file) ? file : [file];
-
-  filesArray.forEach(file => {
+  const files = req.files;
+  
+  const archivos = Object.keys(files) 
+  // return console.log(archivos) => prueba de verificacion 
+  archivos.forEach(file => {
     
-    if(file&&file.name){
-     // const extension = file.name.split('.')[file.name.split('.').length - 1];
-     const extension = path.extname(file.name);
-     const ruta = path.join( __dirname, 'archivos', uuidv4() + '.' + extension)
-     
-    file.mv(ruta ,(error)  => {
-       if(error)
+    const extension = files[file].name.split('.')[files[file].name.split('.').length - 1];
+    const ruta = path.join(__dirname, 'archivos', uuidv4() + '.' + extension)
+
+    files[file].mv(ruta, (error) => {
+      if (error)
         return res.status(500).json({
-        status : 500 ,
-        mensaje : ' Error en el almacenaje del archivo',
-        error
-      })
+          status: 500,
+          mensaje: ' Error en el almacenaje del archivo',
+          error
+        })
+      return res.status(201).json({
+        status: 201,
+        mensaje: ' Archivo almacenado correctamente'
+      });
+
     });
+
+  });
+  /*app.post('/', (req, res)=>{
+    if(!req.files || Object.keys(req.files).length === 0){
+      return res.status(400).send('No se cargó ningún archivo.')
     }
- });
-  return res.status(201).json({
-    status : 201,
-    mensaje : ' Archivo almacenado correctamente'
-});
-/*app.post('/', (req, res)=>{
-  if(!req.files || Object.keys(req.files).length === 0){
-    return res.status(400).send('No se cargó ningún archivo.')
-  }
-   const ArchivoEjemplo = req.files.archivo
-   const extension = ArchivoEjemplo.name.split('.')[ArchivoEjemplo.name.split('.').length -1]
-  
-   const ruta = path.join(__dirname, 'archivos', uuidv4() + '.' + extension)
-   ArchivoEjemplo.mv( ruta, (error) =>{
-     if(error)
-       return res.status(500).json({
-         status : 500 ,
-         mensaje : ' Error en el almacenaje del archivo',
-         error
-  })
-  return res.status(201).json({
-    status : 201,
-    mensaje : ' Archivo almacenado correctamente'
-   })
- })*/
-  
- })
- app.listen(3000 , function() {
-   console.log('servidor es => https://localhost:3000')
+     const ArchivoEjemplo = req.files.archivo
+     const extension = ArchivoEjemplo.name.split('.')[ArchivoEjemplo.name.split('.').length -1]
+    
+     const ruta = path.join(__dirname, 'archivos', uuidv4() + '.' + extension)
+     ArchivoEjemplo.mv( ruta, (error) =>{
+       if(error)
+         return res.status(500).json({
+           status : 500 ,
+           mensaje : ' Error en el almacenaje del archivo',
+           error
+    })
+    return res.status(201).json({
+      status : 201,
+      mensaje : ' Archivo almacenado correctamente'
+     })
+   })*/
+
+})
+app.listen(3000, function () {
+  console.log('servidor es => https://localhost:3000')
 });
